d1c56c97033ef21a820cc61756ef13c225d3ef8c
Soufyane <me@souf.dev>
2022-03-11 10:52:48 +0100

    Updated buffer size

--- Scripts/Set block (buffer) size.lua
+++ Scripts/Set block (buffer) size.lua
@@ -1,8 +1,10 @@
 --@author Souk21
 --@description Set block/buffer size
---@version 1.02
+--@version 1.03
 --@changelog
---   Added menu mode
+--   Added checkmark for current size in menu
+--   Script now exit early if no option is selected from the menu
+--   Code cleanup
 --@metapackage
 --@provides
 --   [main] . > souk21_Set block (buffer) size (menu).lua
@@ -41,16 +43,30 @@ else
   else
     local menu = script_name:match("%(menu%)$") == "(menu)"
     if menu then
+        local retval, current_size = reaper.GetAudioDeviceInfo("BSIZE")
+        if retval then current_size = tonumber(current_size) end
         local menu_items = {
-          {"#Set block size"},
-          {"64"},
-          {"128"},
-          {"256"},
-          {"512"},
-          {"1024"},
-          {"2048"},
-          {"Prompt"},
+          {"#Set block size", nil},
         }
+
+        local buff = 32;
+        local current_added = false;
+        
+        for i = 0, 5 do
+          buff = buff * 2;
+          if retval and not current_added and current_size < buff then
+            table.insert(menu_items, {"!"..tostring(current_size), tostring(current_size)})
+            current_added = true;
+          end
+          if current_size ~= buff then
+            table.insert(menu_items, {tostring(buff),tostring(buff)})
+          end
+        end
+        if retval and not current_added and current_size > buff then
+          table.insert(menu_items, {"!"..tostring(current_size), tostring(current_size)})
+        end
+
+        table.insert(menu_items, {"Prompt", nil})
         local menu = ""
         for i = 1, #menu_items do
           menu = menu .. menu_items[i][1] .. "|"
@@ -64,10 +80,12 @@ else
         gfx.x, gfx.y = gfx.mouse_x-52, gfx.mouse_y-70
         local selection = gfx.showmenu(menu)
         gfx.quit()
-        if selection == 8 then
+        if selection == 0 then
+          return
+        elseif selection == #menu_items then
           prompt = true
         elseif selection ~= 0 then
-          size = tostring(menu_items[selection][1])
+          size = menu_items[selection][2]
         end
     else
       prompt = true
@@ -76,7 +94,7 @@ else
 end
 
 if prompt then
-  local retval;
+  local retval
   retval, size = reaper.GetUserInputs("Set block size", 1, "","")
   if not retval then return end
 end
@@ -86,8 +104,8 @@ reaper.Main_OnCommand(40099, 0) -- Open audio device preferences
 local window = reaper.JS_Window_Find("REAPER Preferences", true)
 
 if window ~= nil then
-  local block_hwnd_asio
-  local block_hwnd_other
+  local hwnd_asio
+  local hwnd_other
   local use_asio = true
   local arr = reaper.new_array({}, 255)
   local count = reaper.JS_Window_ArrayAllChild(window, arr)
@@ -98,27 +116,27 @@ if window ~= nil then
     local title = reaper.JS_Window_GetTitle(hwnd)
     local id = reaper.JS_Window_GetLong(hwnd, "ID")
     if id == 1008 then
-      block_hwnd_asio = hwnd;
+      hwnd_asio = hwnd
     end
     if id == 1009 then
-      block_hwnd_other = hwnd;
+      hwnd_other = hwnd
     end
     if id == 1000 then
-      local protocol = reaper.JS_Window_GetTitle(hwnd);
+      local protocol = reaper.JS_Window_GetTitle(hwnd)
       if protocol == "WaveOut" 
       or protocol == "DirectSound" 
       or protocol == "WDM Kernel Streaming (Windows XP)" 
       or protocol == "WASAPI (Windows 7/8/10/Vista)" 
       or protocol == "Dummy Audio" then
-        use_asio = false;
+        use_asio = false
       end
     end
   end
   
   if use_asio then
-    reaper.JS_Window_SetTitle(block_hwnd_asio, size)
+    reaper.JS_Window_SetTitle(hwnd_asio, size)
   else
-    reaper.JS_Window_SetTitle(block_hwnd_other, size)
+    reaper.JS_Window_SetTitle(hwnd_other, size)
   end
   reaper.JS_WindowMessage_Send(window, "WM_COMMAND", 1144, 0, 0, 0) -- Apply
   reaper.JS_Window_Destroy(window) -- Close window
